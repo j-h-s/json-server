d24bb3a2511e887c5c4348467917d494a2b08db3
J <j.harrissmall@gmail.com>
2019-09-09 19:04:46 +0200

    add input logging functionality

--- server/src/controllers/InputController.js
+++ server/src/controllers/InputController.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
+const moment = require('moment')
 const fileDir = './../public/files/'
+const logFile = './log/input.log'
 
 module.exports = {
   submitJson (req, res) {
@@ -18,10 +20,20 @@ module.exports = {
   }
 }
 
+
 const logJson = (body) => {
-  const array = JSON.stringify(body.input).split(/\n/)
-  console.log(array)
+  const input = body.input.split(/\n/)
+
+  let output = '\n[ ' + moment().format() + ' ] ' + body.name
+  input.map((str) => {
+    output += '\n[ ' + moment().format() + ' ] ' + str
+  })
+
+  fs.appendFile(logFile, output, 'utf-8', (err) => {
+    if (err) throw err
+  })
 }
+
 
 const outputJson = (body) => {
   const pretty = JSON.stringify(JSON.parse(body.input), null, 2)
